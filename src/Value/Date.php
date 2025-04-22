b61bdc0be4ad11080984b12a7035e4bccf4ebf40
Albert Stepantsev <albert.stepantsev@sprintf.ru>
2025-04-22 14:25:23 +0300

    Value\Date development

--- src/Value/Date.php
+++ src/Value/Date.php
@@ -8,10 +8,28 @@ class Date extends \DateTime implements \Stringable
 
     public const string RUSSIAN_DATE = 'd.m.Y';
 
+    public function __construct(string $date = 'today', ?\DateTimeZone $timezone = null)
+    {
+        parent::__construct($date, $timezone);
+        $this->nullTime();
+    }
+
     #[\ReturnTypeWillChange]
     public static function createFromFormat($format, $datetime, $timezone = null): self|false
     {
-        return parent::createFromFormat($format, $datetime, $timezone);
+        $date = parent::createFromFormat($format, $datetime, $timezone);
+        if (false === $date) {
+            return false;
+        }
+        $date->nullTime();
+
+        return $date;
+    }
+
+    #[\ReturnTypeWillChange]
+    public static function createFromImmutable(\DateTimeImmutable $object): self
+    {
+        return parent::createFromImmutable($object)->nullTime();
     }
 
     #[\ReturnTypeWillChange]
@@ -22,6 +40,17 @@ class Date extends \DateTime implements \Stringable
 
     #[\ReturnTypeWillChange]
     public function setTime($hour, $minute, $second = 0, $microsecond = 0): self
+    {
+        throw new \BadMethodCallException();
+    }
+
+    private function nullTime(): self
+    {
+        return parent::setTime(0, 0, 0, 0);
+    }
+
+    #[\ReturnTypeWillChange]
+    public function setMicrosecond($microsecond = 0): self
     {
         throw new \BadMethodCallException();
     }
--- tests/Unit/Value/DateTest.php
+++ tests/Unit/Value/DateTest.php
@@ -3,6 +3,7 @@
 namespace SprintF\Bundle\Datetime\Tests\Unit\Value;
 
 use SprintF\Bundle\Datetime\Tests\Support\UnitTester;
+use SprintF\Bundle\Datetime\Value\Date;
 
 class DateTest extends \Codeception\Test\Unit
 {
@@ -13,8 +14,66 @@ class DateTest extends \Codeception\Test\Unit
     }
 
     // tests
-    public function testTrivial()
+    public function testCreateConstruct()
     {
-        $this->assertTrue(true);
+        $date = new Date();
+        $this->assertInstanceOf(Date::class, $date);
+        $today = new \DateTime('now');
+        $this->assertSame($today->format('Y').'-'.$today->format('m').'-'.$today->format('d').'T00:00:00.000+00:00', $date->format(Date::RFC3339_EXTENDED));
+
+        $date = new Date('now');
+        $this->assertInstanceOf(Date::class, $date);
+        $today = new \DateTime('now');
+        $this->assertSame($today->format('Y').'-'.$today->format('m').'-'.$today->format('d').'T00:00:00.000+00:00', $date->format(Date::RFC3339_EXTENDED));
+
+        $this->expectException(\DateMalformedStringException::class);
+        $today = new \DateTime('wrong date');
+    }
+
+    public function testCreateFromFormat()
+    {
+        $date = Date::createFromFormat('Y-m-d', '2001-02-03');
+        $this->assertInstanceOf(Date::class, $date);
+
+        $this->assertSame('2001-02-03T00:00:00.000+00:00', $date->format(Date::RFC3339_EXTENDED));
+
+        $date = Date::createFromFormat('Y-m-d', 'wrong date');
+        $this->assertFalse($date);
+    }
+
+    public function testCreateFromInterface()
+    {
+        $date = Date::createFromInterface(new \DateTime('2001-02-03'));
+        $this->assertInstanceOf(Date::class, $date);
+
+        $this->assertSame('2001-02-03T00:00:00.000+00:00', $date->format(Date::RFC3339_EXTENDED));
+    }
+
+    public function testCreateFromImmutable()
+    {
+        $date = Date::createFromImmutable(new \DateTimeImmutable('2001-02-03'));
+        $this->assertInstanceOf(Date::class, $date);
+
+        $this->assertSame('2001-02-03T00:00:00.000+00:00', $date->format(Date::RFC3339_EXTENDED));
+    }
+
+    public function testSetTime()
+    {
+        $this->expectException(\BadMethodCallException::class);
+        $date = new Date();
+        $date->setTime(12, 34);
+    }
+
+    public function testSetMicrosecond()
+    {
+        $this->expectException(\BadMethodCallException::class);
+        $date = new Date();
+        $date->setMicrosecond(12);
+    }
+
+    public function testToString()
+    {
+        $date = new Date();
+        $this->assertSame((new \DateTime('today'))->format('Y-m-d'), (string) $date);
     }
 }
