2fc034b82f589c44edf619f3ad709d007656ad51
Albert Stepantsev <albert.stepantsev@sprintf.ru>
2025-04-22 18:48:57 +0300

    Value\DateRange development

---
+++ src/SprintFDatetimeBundle.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace SprintF\Bundle\Datetime;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class SprintFDatetimeBundle extends AbstractBundle
+{
+    public function getPath(): string
+    {
+        return dirname(__DIR__);
+    }
+
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+    }
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        // $container->import('../config/services.yaml');
+    }
+}
---
+++ tests/Unit/Value/DateRangeTest.php
@@ -0,0 +1,180 @@
+<?php
+
+namespace SprintF\Bundle\Datetime\Tests\Unit\Value;
+
+use SprintF\Bundle\Datetime\Tests\Support\UnitTester;
+use SprintF\Bundle\Datetime\Value\Date;
+use SprintF\Bundle\Datetime\Value\DateRange;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+class DateRangeTest extends \Codeception\Test\Unit
+{
+    protected UnitTester $tester;
+
+    protected function _before()
+    {
+    }
+
+    // tests
+    public function testCreateConstruct()
+    {
+        // Бесконечный всюду диапазон
+        $dateRange = new DateRange(null, null);
+        $this->assertInstanceOf(DateRange::class, $dateRange);
+        $this->assertSame(null, $dateRange->getBeginDate());
+        $this->assertSame(null, $dateRange->getEndDate());
+
+        // Бесконечный вправо диапазон
+        $dateRange = new DateRange(new Date('today'), null);
+        $this->assertInstanceOf(DateRange::class, $dateRange);
+        $this->assertEquals(new Date('today'), $dateRange->getBeginDate());
+        $this->assertSame(null, $dateRange->getEndDate());
+
+        // Бесконечный влево диапазон
+        $dateRange = new DateRange(null, new Date('today'));
+        $this->assertInstanceOf(DateRange::class, $dateRange);
+        $this->assertSame(null, $dateRange->getBeginDate());
+        $this->assertEquals(new Date('today'), $dateRange->getEndDate());
+
+        // Диапазон из одной даты
+        $dateRange = new DateRange(new Date('today'), new Date('today'));
+        $this->assertInstanceOf(DateRange::class, $dateRange);
+        $this->assertEquals(new Date('today'), $dateRange->getBeginDate());
+        $this->assertEquals(new Date('today'), $dateRange->getEndDate());
+
+        // Диапазон из нескольких дат
+        $dateRange = new DateRange(new Date('today'), new Date('first day of next year'));
+        $this->assertInstanceOf(DateRange::class, $dateRange);
+        $this->assertEquals(new Date('today'), $dateRange->getBeginDate());
+        $this->assertEquals(new Date('first day of next year'), $dateRange->getEndDate());
+
+        // Некорректный диапазон
+        $this->expectException(\InvalidArgumentException::class);
+        $dateRange = new DateRange(new Date('first day of next year'), new Date('today'));
+    }
+
+    public function testNormalization()
+    {
+        /*
+         * Тест пока не реализован. Нет возможности корректно запустить нормалайзер вне приложения Symfony.
+         *
+        $encoders = [new JsonEncoder()];
+        $normalizers = [new DateTimeNormalizer(), new ObjectNormalizer()];
+        $serializer = new Serializer($normalizers, $encoders);
+
+        // Бесконечный всюду диапазон
+        $dateRange = new DateRange(null, null);
+        $this->assertSame(['beginDate' => null, 'endDate' => null], $serializer->normalize($dateRange));
+
+        // Бесконечный вправо диапазон
+        $today = new Date('today');
+        $Ymd = $today->format('Y-m-d');
+        $dateRange = new DateRange($today, null);
+        $this->assertSame(['beginDate' => $Ymd, 'endDate' => null], $serializer->normalize($dateRange));
+        */
+    }
+
+    public function testFromStringInfinite()
+    {
+        $dateRange = DateRange::fromString('[,)');
+        $this->assertInstanceOf(DateRange::class, $dateRange);
+        $this->assertSame(null, $dateRange->getBeginDate());
+        $this->assertSame(null, $dateRange->getEndDate());
+
+        $dateRange = DateRange::fromString('(,)');
+        $this->assertInstanceOf(DateRange::class, $dateRange);
+        $this->assertSame(null, $dateRange->getBeginDate());
+        $this->assertSame(null, $dateRange->getEndDate());
+
+        $dateRange = DateRange::fromString('[,]');
+        $this->assertInstanceOf(DateRange::class, $dateRange);
+        $this->assertSame(null, $dateRange->getBeginDate());
+        $this->assertSame(null, $dateRange->getEndDate());
+    }
+
+    public function testFromStringInfiniteRight()
+    {
+        $dateRange = DateRange::fromString('[2021-02-03,)');
+        $this->assertInstanceOf(DateRange::class, $dateRange);
+        $this->assertEquals(new Date('2021-02-03'), $dateRange->getBeginDate());
+        $this->assertSame(null, $dateRange->getEndDate());
+
+        $dateRange = DateRange::fromString('(2021-02-03,)');
+        $this->assertInstanceOf(DateRange::class, $dateRange);
+        $this->assertEquals(new Date('2021-02-04'), $dateRange->getBeginDate());
+        $this->assertSame(null, $dateRange->getEndDate());
+    }
+
+    public function testFromStringInfiniteLeft()
+    {
+        $dateRange = DateRange::fromString('[,2021-02-03)');
+        $this->assertInstanceOf(DateRange::class, $dateRange);
+        $this->assertSame(null, $dateRange->getBeginDate());
+        $this->assertEquals(new Date('2021-02-03'), $dateRange->getEndDate());
+
+        $dateRange = DateRange::fromString('[,2021-02-03]');
+        $this->assertInstanceOf(DateRange::class, $dateRange);
+        $this->assertSame(null, $dateRange->getBeginDate());
+        $this->assertEquals(new Date('2021-02-04'), $dateRange->getEndDate());
+    }
+
+    public function testFromStringFinite()
+    {
+        $dateRange = DateRange::fromString('[2021-01-02,2021-02-03)');
+        $this->assertInstanceOf(DateRange::class, $dateRange);
+        $this->assertEquals(new Date('2021-01-02'), $dateRange->getBeginDate());
+        $this->assertEquals(new Date('2021-02-03'), $dateRange->getEndDate());
+
+        $dateRange = DateRange::fromString('(2021-01-02,2021-02-03)');
+        $this->assertInstanceOf(DateRange::class, $dateRange);
+        $this->assertEquals(new Date('2021-01-03'), $dateRange->getBeginDate());
+        $this->assertEquals(new Date('2021-02-03'), $dateRange->getEndDate());
+
+        $dateRange = DateRange::fromString('(2021-01-02,2021-02-03]');
+        $this->assertInstanceOf(DateRange::class, $dateRange);
+        $this->assertEquals(new Date('2021-01-03'), $dateRange->getBeginDate());
+        $this->assertEquals(new Date('2021-02-04'), $dateRange->getEndDate());
+
+        $dateRange = DateRange::fromString('[2021-01-02,2021-02-03]');
+        $this->assertInstanceOf(DateRange::class, $dateRange);
+        $this->assertEquals(new Date('2021-01-02'), $dateRange->getBeginDate());
+        $this->assertEquals(new Date('2021-02-04'), $dateRange->getEndDate());
+
+        $this->expectException(\InvalidArgumentException::class);
+        $dateRange = DateRange::fromString('[2021-02-03,2021-01-02]');
+
+        $this->expectException(\InvalidArgumentException::class);
+        $dateRange = DateRange::fromString('[something,wrong]');
+    }
+
+    public function testFromStringInvalid()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $dateRange = DateRange::fromString('[something,wrong]');
+    }
+
+    public function testContainsDate()
+    {
+        $today = new Date('today');
+        $dateToday = $today->format('Y-m-d');
+        $dateInPast = '2000-01-01';
+        $dateInFuture = (new Date('first day of next year'))->format('Y-m-d');
+
+        $this->assertTrue(DateRange::fromString('[,)')->containsDate($today));
+
+        $this->assertTrue(DateRange::fromString('['.$dateInPast.',)')->containsDate($today));
+        $this->assertTrue(DateRange::fromString('['.$dateToday.',)')->containsDate($today));
+        $this->assertFalse(DateRange::fromString('['.$dateInFuture.',)')->containsDate($today));
+
+        $this->assertFalse(DateRange::fromString('[,'.$dateInPast.')')->containsDate($today));
+        $this->assertFalse(DateRange::fromString('[,'.$dateToday.')')->containsDate($today));
+        $this->assertTrue(DateRange::fromString('[,'.$dateInFuture.')')->containsDate($today));
+
+        $this->assertFalse(DateRange::fromString('['.$dateInPast.','.$dateToday.')')->containsDate($today));
+        $this->assertTrue(DateRange::fromString('['.$dateToday.','.$dateInFuture.')')->containsDate($today));
+        $this->assertTrue(DateRange::fromString('['.$dateInPast.','.$dateInFuture.')')->containsDate($today));
+    }
+}
